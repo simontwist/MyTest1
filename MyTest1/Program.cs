fb7f2cb392c8ffc0b3b57c6d2d965c3280e14006
simontwist <simon.twist@signumcorporate.com>
2016-06-17 16:26:14 +0100

    update comments for patch 2

--- MyTest1/Program.cs
+++ MyTest1/Program.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +11,7 @@ namespace MyTest1
         static void Main(string[] args)
         {
             //Hi
+            //Patch 2 comment
         }
     }
 }
